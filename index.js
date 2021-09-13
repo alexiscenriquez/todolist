b5dbf217fd206f24b032d54d2e60a65d4bea5da9
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-09-13 03:15:36 -0500

    js progress

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-let add=document.querySelector("#add");
-let del=document.querySelector("#delete")
-let input=document.querySelector("#item")
-let list=document.querySelector("#items")
-let checkbox=document.forms[0];
-let err=document.querySelector("#error")
-let items=[]
-add.addEventListener("click",()=>{
+let add = document.querySelector("#add");
+let del = document.querySelector("#delete");
+let input = document.querySelector("#item");
+let list = document.querySelector("#items");
+let checkbox = document.forms[0];
+let err = document.querySelector("#error");
+let items = [];
+let newItems = [];
+add.addEventListener("click", () => {
+  if (input.value.length == 0) {
+    err.style.visibility = "visible";
+  } else {
+    list.innerHTML += `<li> <label for="check">${input.value}</label> <input type="checkbox" class="check" name="check"></li>`;
+    items.push(input.value);
+    input.value = "";
+  }
+});
 
-if(input.value.length==0){
-err.style.visibility="visible"}
-else{
-    list.innerHTML+=`<li> <label for="check">${input.value}</label> <input type="checkbox" class="check" name="check"></li>`
-items.push(input.value)
-input.value=""
-}
-})
-del.addEventListener("click",()=>{
-    
-    for(let i=0;i<checkbox.length;i++){
-        if(checkbox[i].checked){
-           items.splice(i,1)
-          
-        }
-        if(checkbox.length===1&&checkbox[i].checked){
-            items=[]
-            list.innerHTML=""
-        }
+del.addEventListener("click", () => {
+  for (let i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked) {
+      items.splice(i,1);
     }
-
-    for(let i=0;i<items.length;i++){
-       
-        list.innerHTML=""
-        list.innerHTML+=`<li> <label for="check">${items[i]}</label> <input type="checkbox" class="check" name="check"></li>`
-        if(items.length<=1 &&checkbox[i].checked){
-            list.innerHTML=""
-        }
-    }
-})
+  }
+  list.innerHTML = "";
+  for (let i = 0; i <items.length; i++) {
+ list.innerHTML += `<li><label for="check">${items[i]}</label> <input type="checkbox" class="check" name="check"></li>`;
+  }
+});
