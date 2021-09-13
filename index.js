5cd6e5ab0741b13d1dce38cabbb81f104b2ba096
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-09-13 14:21:31 -0500

    fixed delete function

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ let err = document.querySelector("#error");
 let items = [];
 let newItems = [];
 add.addEventListener("click", () => {
-  if (input.value.length == 0) {
-    err.style.visibility = "visible";
-  } else {
-    list.innerHTML += `<li> <label for="check">${input.value}</label> <input type="checkbox" class="check" name="check"></li>`;
-    items.push(input.value);
-    input.value = "";
-  }
+    if (input.value.length == 0) {
+        err.style.visibility = "visible";
+    } else {
+        list.innerHTML += `<li> <label for="check">${input.value}</label> <input type="checkbox" class="check" name="check" value=${input.value}></li>`;
+        items.push(input.value);
+        input.value = "";
+    }
 });
 
+
 del.addEventListener("click", () => {
-  for (let i = 0; i < checkbox.length; i++) {
-    if (checkbox[i].checked) {
-      items.splice(i,1);
+    for (let i = checkbox.length-1; i >=0; i--) {
+      if (checkbox[i].checked) {
+        items.splice(i,1);
+      }
     }
-  }
-  list.innerHTML = "";
-  for (let i = 0; i <items.length; i++) {
- list.innerHTML += `<li><label for="check">${items[i]}</label> <input type="checkbox" class="check" name="check"></li>`;
-  }
-});
+    list.innerHTML = "";
+    for (let i = 0; i <items.length; i++) {
+   list.innerHTML += `<li><label for="check">${items[i]}</label> <input type="checkbox" class="check" name="check"></li>`;
+    }
+  });
