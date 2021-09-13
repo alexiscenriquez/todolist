3443684bf1a995379595d3ecbdfd5ce0e45200d3
alexiscenriquez <alexiscenriquez@hotmail.com>
2021-09-13 01:40:53 -0500

    add and delete button progress

--- index.js
+++ index.js
@@ -2,7 +2,35 @@ let add=document.querySelector("#add");
 let del=document.querySelector("#delete")
 let input=document.querySelector("#item")
 let list=document.querySelector("#items")
+let checkbox=document.forms[0];
+let err=document.querySelector("#error")
+let items=[]
 add.addEventListener("click",()=>{
-list.innerHTML+=`<li> ${input.value} <input type="checkbox"></li>`
-})
 
+if(input.value.length==0){
+err.style.visibility="visible"}
+else{
+    list.innerHTML+=`<li> <label for="check">${input.value}</label> <input type="checkbox" class="check" name="check"></li>`
+items.push(input.value)
+input.value=""
+}
+})
+del.addEventListener("click",()=>{
+    
+    for(let i=0;i<checkbox.length;i++){
+        if(checkbox[i].checked){
+           items.splice(i,1)
+          
+        }
+        if(checkbox.length==1&&checkbox.checked){
+            items.shift
+        }
+    }
+
+    for(let i=0;i<items.length;i++){
+       
+        list.innerHTML=""
+        list.innerHTML+=`<li> <label for="check">${items[i]}</label> <input type="checkbox" class="check" name="check"></li>`
+        
+    }
+})
